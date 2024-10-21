ba8c93cfce31f26ea1ca85c21cc1f358177872df
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-21 15:38:20 +0600

    event handler add

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,10 +1,16 @@
-const Bottle = ({ bottle }) => {
+const Bottle = ({ bottle, handleCart }) => {
   const { name, img, price } = bottle;
   return (
     <div className="flex flex-col items-center border-2 border-slate-700 rounded-xl p-10">
       <img className="w-40 rounded-md object-cover" src={img}></img>
       <h3 className="font-bold text-xl pt-4">Name : {name}</h3>
       <p className="font-bold pt-2">Price : {price}</p>
+      <button
+        className="btn btn-success mt-4"
+        onClick={() => handleCart(bottle)}
+      >
+        Precess
+      </button>
     </div>
   );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -4,20 +4,30 @@ import Bottle from "../Bottle/Bottle";
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch("bottles.json")
       .then((res) => res.json())
       .then((data) => setBottles(data));
   }, []);
+  const handleCart = (bottle) => {
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+  };
   console.log(bottles);
   return (
     <div className="pt-10">
       <h2 className="text-4xl font-bold ">
         All Data of Bottles : {bottles.length}
       </h2>
+      <h3 className="pt-5 font-semibold text-2xl">Bottle : {cart.length}</h3>
       <div className="grid grid-cols-3 gap-5 pt-10">
         {bottles.map((bottle) => (
-          <Bottle key={bottle.id} bottle={bottle}></Bottle>
+          <Bottle
+            key={bottle.id}
+            bottle={bottle}
+            handleCart={handleCart}
+          ></Bottle>
         ))}
       </div>
     </div>
