af6482c29aba2ed3ebf9fa97accbdf113264eb50
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-21 13:00:20 +0600

    hearder is add in the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <Header></Header>
+      <h1 className="text-6xl">React</h1>
     </>
   );
 }
